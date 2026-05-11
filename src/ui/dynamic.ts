4a41f216e73ca574e9f5f5153dc3f99c2bb91757
streetraceing <voltzxt@vk.com>
2026-05-11 12:55:05 +0300

    optimizations for dynamic background: new logic, dedupe render

--- src/ui/dynamic.ts
+++ src/ui/dynamic.ts
@@ -5,6 +5,9 @@ export class DynamicBackground {
   private static currentSong: SongPayload | null = null;
   private static currentCanvas: HTMLVideoElement | null = null;
 
+  private static lastRenderKey: string | null = null;
+  private static canvasUnmountTimeout: number | null = null;
+
   static init() {
     Luminous.Song.addEventListener("ready", this.handleSong);
     Luminous.Song.addEventListener("change", this.handleSong);
@@ -15,15 +18,38 @@ export class DynamicBackground {
   }
 
   static render() {
-    if (Luminous.Settings.get("dynamicBackground") === false) return;
-
-    if (this.currentCanvas && !document.hidden) {
-      Luminous.Background.render({ canvas: this.currentCanvas });
+    if (Luminous.Settings.get("dynamicBackground") === false) {
       return;
     }
 
+    let key = "empty";
+
+    if (this.currentCanvas) {
+      key = `canvas:${this.currentCanvas.currentSrc}`;
+    } else if (this.currentSong?.image) {
+      key = `image:${this.currentSong.image}`;
+    }
+
+    if (key === this.lastRenderKey) {
+      return;
+    }
+
+    this.lastRenderKey = key;
+
+    const canvas = Luminous.Canvas.getVideo();
+
+    // canvas always has priority
+    if (canvas) {
+      Luminous.Background.render({ canvas });
+      return;
+    }
+
+    // fallback to image
     if (this.currentSong?.image) {
-      Luminous.Background.render({ image: this.currentSong.image });
+      Luminous.Background.render({
+        image: this.currentSong.image,
+      });
+
       return;
     }
 
@@ -36,19 +62,29 @@ export class DynamicBackground {
   };
 
   private static handleCanvas = (canvas: CanvasPayload) => {
-    if (!canvas.video) return;
-    if (!this.currentSong?.track.metadata["canvas.id"]) return;
+    if (this.canvasUnmountTimeout) {
+      clearTimeout(this.canvasUnmountTimeout);
+      this.canvasUnmountTimeout = null;
+    }
 
     this.currentCanvas = canvas.video;
     this.render();
   };
 
   private static handleCanvasUnmount = () => {
-    if (this.currentSong?.track.metadata["canvas.id"] !== undefined) {
-      return;
+    this.currentCanvas = null;
+
+    if (this.canvasUnmountTimeout) {
+      clearTimeout(this.canvasUnmountTimeout);
     }
 
-    this.currentCanvas = null;
-    this.render();
+    this.canvasUnmountTimeout = window.setTimeout(() => {
+      // new canvas may already appear
+      if (this.currentCanvas) {
+        return;
+      }
+
+      this.render();
+    }, 300);
   };
 }
